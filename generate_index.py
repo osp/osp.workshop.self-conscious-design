8728aab677daa8b064a6e5a1ed92a098a2e23883
Eric Schrijver <eric@ericschrijver.nl>
2014-04-24 00:19:12 +0200

    Generate a more complet index by parsing the metadata file for author, title, description

--- generate_index.py
+++ generate_index.py
@@ -1,5 +1,8 @@
 import os
 import json
+import codecs
+
+from xml.dom.minidom import parse
 
 PROJECT_PATH = os.path.abspath(os.path.dirname(__file__))
 EPUB_DIR = os.path.join(PROJECT_PATH, 'public', 'epub_content')
@@ -17,15 +20,32 @@ def find_package(path):
                 return os.path.join(root, filename).replace(PROJECT_PATH + '/public/', '')
     return False
 
+def parse_meta(path):
+    meta = {}
+    opf = parse(os.path.join('public', path))
+    
+    ti_e = opf.getElementsByTagName('dc:title')
+    meta['title'] = ti_e[0].firstChild.data if len(ti_e) > 0 else u""
+    
+    a_e = opf.getElementsByTagName('dc:creator')
+    meta['author'] = a_e[0].firstChild.data if len(a_e) > 0 else u""
+    
+    d_e = opf.getElementsByTagName('dc:description')
+    meta['description'] = d_e[0].firstChild.data if len(d_e) > 0 else u""
+    
+    return meta
+
 for i in os.listdir(EPUB_DIR):
     if os.path.isdir(os.path.join(EPUB_DIR, i)):
         this_epub_path = os.path.join(EPUB_DIR, i)
         if os.path.exists(os.path.join(EPUB_DIR, i, 'mimetype')):
             package = find_package(this_epub_path)
             if package:
-                obj['library_epubs'].append( { 
-                        'title' : i.replace('_',' ').title(),
-                        'epub_download': os.path.join('epub_content', i, '%s.epub' % i),
-                        'url_to_package_doc' : package } )
+                meta  = parse_meta(package)
+                if not meta['title']:
+                    meta['title'] = i.replace('_',' ').title()
+                meta['epub_download'] = os.path.join('epub_content', i, '%s.epub' % i)
+                meta['url_to_package_doc'] = package
+                obj['library_epubs'].append(meta)
 
-print json.dumps(obj, indent=2, ensure_ascii=False)
+print json.dumps(obj, indent=2, ensure_ascii=False).encode('utf-8')
